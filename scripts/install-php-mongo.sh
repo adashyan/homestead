23617442363154f2319b5bc34cf6a89972780878
Artak Dashyan <ar@iMac-2.local>
2016-12-28 14:20:45 -0500

    testing

--- scripts/install-php-mongo.sh
+++ scripts/install-php-mongo.sh
@@ -1,24 +1,24 @@
-#!/usr/bin/env bash
-# Check If php-redis Has Been Installed
-
-if [ -f /home/vagrant/.php-mongo ]
-then
-   echo "php-mongo already installed."
-   exit 0
-fi
-
-touch /home/vagrant/.php-mongo
-
-yes no | sudo pecl install mongo
-
-
-extension=mongo.so
-
-touch /etc/php5/mods-available/mongo.ini
-echo "extension=mongo.so" > /etc/php5/mods-available/mongo.ini
-
-ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/20-mongo.ini
-ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/20-mongo.ini
-
-sudo /etc/init.d/php5-fpm restart
-sudo /etc/init.d/nginx restart
+##!/usr/bin/env bash
+## Check If php-redis Has Been Installed
+#
+#if [ -f /home/vagrant/.php-mongo ]
+#then
+#   echo "php-mongo already installed."
+#   exit 0
+#fi
+#
+#touch /home/vagrant/.php-mongo
+#
+#yes no | sudo pecl install mongo
+#
+#
+#extension=mongo.so
+#
+#touch /etc/php5/mods-available/mongo.ini
+#echo "extension=mongo.so" > /etc/php5/mods-available/mongo.ini
+#
+#ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/20-mongo.ini
+#ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/20-mongo.ini
+#
+#sudo /etc/init.d/php5-fpm restart
+#sudo /etc/init.d/nginx restart
