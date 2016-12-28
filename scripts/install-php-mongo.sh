47b941c4c7da45f6a9347f6cc5c5b4adfa8a3bd8
Artak Dashyan <ar@iMac-2.local>
2016-12-28 15:28:22 -0500

    testing

--- scripts/install-php-mongo.sh
+++ scripts/install-php-mongo.sh
@@ -1,22 +1,25 @@
-##!/usr/bin/env bash
-## Check If php-mongo Has Been Installed
-#
-#if [ -f /home/vagrant/.php-mongo ]
-#then
-#   echo "php-mongo already installed."
-#   exit 0
-#fi
-#
-#touch /home/vagrant/.php-mongo
-#
-#echo "Installing php mongodb driver"
-#yes no | sudo pecl install mongo
+#!/usr/bin/env bash
+# Check If php-mongo Has Been Installed
 
-#touch /etc/php5/mods-available/mongo.ini
-#echo "extension=mongo.so" > /etc/php5/mods-available/mongo.ini
-#
-#ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/20-mongo.ini
-#ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/20-mongo.ini
-#
-#sudo /etc/init.d/php5-fpm restart
-#sudo /etc/init.d/nginx restart
+if [ -f /home/vagrant/.php-mongo ]
+then
+   echo "php-mongo already installed."
+   exit 0
+fi
+
+touch /home/vagrant/.php-mongo
+
+echo "upgrading nginx binary"
+sudo /etc/init.d/nginx upgrade
+
+echo "Installing php mongodb driver"
+yes no | sudo pecl install mongo
+
+touch /etc/php5/mods-available/mongo.ini
+echo "extension=mongo.so" > /etc/php5/mods-available/mongo.ini
+
+ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/20-mongo.ini
+ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/20-mongo.ini
+
+sudo /etc/init.d/nginx restart
+sudo /etc/init.d/php5-fpm restart
