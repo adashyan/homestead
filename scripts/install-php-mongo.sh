0bc0adff16e45b8ba1bbe385612b50d6b5980721
Artak Dashyan <ar@iMac-2.local>
2016-12-28 15:48:37 -0500

    testing

--- scripts/install-php-mongo.sh
+++ scripts/install-php-mongo.sh
@@ -15,11 +15,14 @@ sudo /etc/init.d/nginx upgrade
 echo "Installing php mongodb driver"
 yes no | sudo pecl install mongo
 
+echo "adding 'extension=mongo.so' into php.ini"
 touch /etc/php5/mods-available/mongo.ini
 echo "extension=mongo.so" > /etc/php5/mods-available/mongo.ini
 
 ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/fpm/conf.d/20-mongo.ini
 ln -sf /etc/php5/mods-available/mongo.ini /etc/php5/cli/conf.d/20-mongo.ini
 
+echo "restarting nginx and php5-fpm"
+sudo service php5-fpm restart
 sudo /etc/init.d/nginx restart
-sudo /etc/init.d/php5-fpm restart
+#sudo /etc/init.d/php5-fpm restart
