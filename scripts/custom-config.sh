3090ae5b8a7bfcd9a14a82df77e52d1081997b50
Artak Dashyan <a.dashyan@hotmail.com>
2017-04-13 10:02:59 -0400

    Update custom-config.sh

--- scripts/custom-config.sh
+++ scripts/custom-config.sh
@@ -10,12 +10,10 @@ touch /home/vagrant/.custom-config
 echo "
 xdebug.var_display_max_depth = -1
 xdebug.var_display_max_children = -1
-xdebug.var_display_max_data = -1
-" >> "/etc/php/7.0/fpm/conf.d/20-xdebug.ini"
+xdebug.var_display_max_data = -1" >> "/etc/php/7.0/fpm/conf.d/20-xdebug.ini"
 
 echo "
 xdebug.profiler_enable=0
 xdebug.profiler_enable_trigger = 1;
 xdebug.profiler_output_name = cachegrind.out.%p.%t
-xdebug.profiler_output_dir = /home/vagrant/Sites/cachegrind
-" >> "/etc/php/7.0/fpm/conf.d/20-xdebug.ini"
+xdebug.profiler_output_dir = /home/vagrant/Sites/cachegrind" >> "/etc/php/7.0/fpm/conf.d/20-xdebug.ini"
